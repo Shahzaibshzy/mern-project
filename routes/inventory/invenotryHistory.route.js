bd746252f0aed13c4ea5b13932407cc1d8979d69
Shahzaibshzy <112001025+Shahzaibshzy@users.noreply.github.com>
2024-11-29 22:11:37 +0500

    Delete routes directory

--- routes/inventory/invenotryHistory.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const inventoryController = require("./inventoryHistory.controller");
-
-router.get("/", inventoryController.getInventoryHistory);
-router.get("/:id", inventoryController.getInventoryHistoryById);
-router.delete("/:id", inventoryController.deleteHistoryById);
-
-module.exports = router;
