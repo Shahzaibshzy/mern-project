e867c322a09dd2f7e60f9a3dce88e086d3444b80
Shahzaibshzy <shhzbsaleem@gmail.com>
2024-12-04 01:35:17 +0500

    new project check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,7 @@
-import React from "react";
-import Home from "../src/components/page";
-
-function App() {
-  return <div className="App">
-    <Home/>
-  </div>;
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind!</h1>
+    </div>
+  );
 }
-
-export default App;
